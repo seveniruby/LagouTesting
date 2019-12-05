2407187fb2f06acff4d048f1b842271522b73055
seveniruby <seveniruby@gmail.com>
2019-12-05 16:42:06 +0800

    备课

--- demo.py
+++ demo.py
@@ -1,1 +1,18 @@
+from collections import deque
+
 print("hello world")
+
+x="xxx"
+print(f"{x} ddd")
+l=[1,2, "3", "456"]
+print(l)
+l.append("7")
+l.remove(2)
+print(l)
+
+ln=deque(l)
+ln.popleft()
+
+
+print(l)
+print(ln)
