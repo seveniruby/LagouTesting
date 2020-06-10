64e888065bb93cca7df37673d59d45db28d31673
seveniruby <seveniruby@gmail.com>
2020-06-10 19:48:26 +0800

    更新url

--- test_http/test_http.py
+++ test_http/test_http.py
@@ -4,7 +4,7 @@ import requests
 class TestHttp:
     def test_get(self):
         r = requests.get(
-            "https://httpbin.testing-studio.com/get",
+            "https://httpbin.ceshiren.com/get",
             params={'a': 1, 'b': '霍格沃兹测试学院'}
         )
         print(r.status_code)
@@ -15,10 +15,10 @@ class TestHttp:
 
     def test_post(self):
         r=requests.post(
-            'https://httpbin.testing-studio.com/post',
+            'https://httpbin.ceshiren.com/post',
             data={"custtel": '15600534760'}
         )
         print(r.text)
         assert r.status_code == 200
-        # assert r.json()['url'] == 'https://httpbin.testing-studio.com/post'
+        # assert r.json()['url'] == 'https://httpbin.ceshiren.com/post'
         assert r.json()['form']['custtel'] == '15600534760'
